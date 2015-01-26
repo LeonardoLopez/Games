e7a94f0c71565cf9fa647a251eb8b65221f665e0
LeonardoLopez <g.lelopez1@gmail.com>
2015-01-26 16:09:29 +0100

    Initial commit

---
+++ app/src/main/java/com/example/leonardolopez/games/Helicopter1.java
@@ -0,0 +1,18 @@
+package com.example.leonardolopez.games;
+
+import sheep.game.Game;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Helicopter1 extends Activity {
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Create the game.
+        Game game = new Game(this, null);
+        // Push the main state.
+        game.pushState(new Heli1());
+        // View the game.
+        setContentView(game);
+    }
+}
