95e92077ef79ed3bbcbefbdda0ccd5ee890fa956
Sandipan Das <sandipad@stud.ntnu.no>
2015-01-28 11:44:50 +0100

    Problem 1

--- app/src/main/java/com/example/leonardolopez/games/activities/Helicopter1.java
+++ app/src/main/java/com/example/leonardolopez/games/activities/Helicopter1.java
@@ -4,7 +4,7 @@ import sheep.game.Game;
 import android.app.Activity;
 import android.os.Bundle;
 
-import com.example.leonardolopez.games.states.Heli1;
+import com.example.leonardolopez.games.states.Heli1State;
 
 public class Helicopter1 extends Activity {
 
@@ -15,7 +15,7 @@ public class Helicopter1 extends Activity {
         // Create the game.
         Game game = new Game(this, null);
         // Push the main state.
-        game.pushState(new Heli1());
+        game.pushState(new Heli1State(game.getResources(), this.getBaseContext()));
         // View the game.
         setContentView(game);
     }
---
+++ app/src/main/java/com/example/leonardolopez/games/states/Heli1State.java
@@ -0,0 +1,53 @@
+package com.example.leonardolopez.games.states;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import com.example.leonardolopez.games.R;
+import com.example.leonardolopez.games.models.HeliModel;
+import sheep.game.State;
+
+
+public class Heli1State extends State {
+    private HeliModel heli1;
+    private int canvasHeight, canvasWidth;
+    private int scrnW;
+    private int scrnH;
+
+    public Heli1State(Resources resources, Context context) {
+
+        this.scrnW= context.getResources().getDisplayMetrics().widthPixels;
+        this.scrnH= context.getResources().getDisplayMetrics().heightPixels;
+
+        heli1 = new HeliModel(BitmapFactory.decodeResource(resources, R.drawable.helisprite),0,200,100,4);
+        heli1.setSpeed(1000, -1000);
+        heli1.update(System.currentTimeMillis());
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        canvasHeight = canvas.getHeight();
+        canvasWidth = canvas.getWidth();
+        canvas.drawColor(Color.BLACK);
+        heli1.draw(canvas);
+        //Font font = new Font(0, 55, 20, 30, Typeface.SERIF, Typeface.NORMAL);
+        //canvas.drawText("Helicopter 1 Position (X:" + heli1.getX() + ", Y:" + heli1.getY() +")", 30, this.scrnH-160, font);
+    }
+
+    @Override
+    public void update(float dt) {
+        heli1.update(System.currentTimeMillis());
+
+        if(heli1.getX()>(canvasWidth- heli1.getHelicopterWidth()) || heli1.getX()<0)
+        {
+            heli1.setSpeed(-heli1.getSpeed().getX(), heli1.getSpeed().getY());
+            heli1.flipHelicopter();
+        }
+        if(heli1.getY()>(canvasHeight- heli1.getHelicopterHeight()) || heli1.getY()<0)
+        {
+            heli1.setSpeed(heli1.getSpeed().getX(), -heli1.getSpeed().getY());
+        }
+    }
+}
